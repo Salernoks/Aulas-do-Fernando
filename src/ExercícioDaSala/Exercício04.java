2a6b3a42b574c097bc0b3e073c85c7c7b2196297
vitor.scsilva1 <vitor.scsilva1@CAS555592W0739.senacsp.edu.br>
2017-03-24 22:08:54 -0300

    Aula de Algoritmo 24.03.17

---
+++ src/ExercícioDaSala/Exercício04.java
@@ -0,0 +1,12 @@
+package ExercícioDaSala;
+
+import java.util.Scanner;
+
+public class Exercício04 {
+    public static void main (String [] args){
+        Scanner leitro = new Scanner (System.in);
+        
+        
+    }
+    
+}
